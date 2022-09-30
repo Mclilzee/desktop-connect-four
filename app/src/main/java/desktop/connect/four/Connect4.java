fa3b7ef920652e2380dbebc5b69f8e0bbb8136d7
Mclilzee <mc.lilzee@hotmail.com>
2022-09-30 15:53:21 +0200

    Gradle init

---
+++ app/src/main/java/desktop/connect/four/Connect4.java
@@ -0,0 +1,14 @@
+package desktop.connect.four;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Connect4 extends JFrame {
+
+    public Connect4(String title) {
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+}
