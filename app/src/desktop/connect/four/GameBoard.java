fbd9d05a6904c9d4f1b5e566c446fb5b6ba414d3
Emad Ali <mc.lilzee@hotmail.com>
2022-10-05 18:45:52 +0200

    Add row winning condition functionality

--- app/src/desktop/connect/four/GameBoard.java
+++ app/src/desktop/connect/four/GameBoard.java
@@ -28,10 +28,10 @@ public class GameBoard {
     }
 
     public void addButtonsToPanel() {
-        for (int i = rows - 1; i >= 0; i--) {
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 JButton button = new JButton(" ");
-                button.setName("Button" + ((char) (j + 65)) + (i + 1));
+                button.setName("Button" + ((char) (j + 65)) + (rows - i));
                 button.setBackground(Color.gray);
                 button.setFocusPainted(false);
                 button.addActionListener(e -> insertInFirstColumnElement(button));
@@ -49,35 +49,58 @@ public class GameBoard {
 
         int buttonColumnIndex = button.getName().charAt(6) - 65;
 
-        for (int i = 0; i < rows; i++) {
+        for (int i = rows - 1; i >= 0; i--) {
             JButton eachButton = buttonsBoard[i][buttonColumnIndex];
             if (eachButton.getText().isBlank()) {
                 eachButton.setText(player);
                 player = player.equals("X") ? "O" : "X";
+                checkIfGameOver(i, buttonColumnIndex);
                 break;
             }
         }
 
-//        checkIfGameOver();
     }
 
-//    private void checkIfGameOver() {
-//        if (rowsConditionMet() || columnsConditionMet() || diagonalLeftConditionMet() || diagonalRightConditionMet()) {
-//            gameOver = true;
-//        };
-//    }
-//
-//    private boolean rowsConditionMet() {
-//
-//    }
+    private void checkIfGameOver(int row, int column) {
+        if (rowsConditionMet(row, column)) {
+            gameOver = true;
+        };
+    }
+
+    private boolean rowsConditionMet(int row, int column) {
+
+
+        for (int i = row - 3; i < this.rows - 3; i++) {
+            if (i < 0) {
+                continue;
+            }
+
+            JButton first = buttonsBoard[i][column];
+            JButton second = buttonsBoard[i + 1][column];
+            JButton third = buttonsBoard[i + 2][column];
+            JButton fourth = buttonsBoard[i + 3][column];
+
+            if (!first.getText().isBlank() && first.getText().equals(second.getText()) && first.getText().equals(third.getText()) && first.getText().equals(fourth.getText())) {
+                first.setBackground(Color.GREEN);
+                second.setBackground(Color.GREEN);
+                third.setBackground(Color.GREEN);
+                fourth.setBackground(Color.GREEN);
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public void reset() {
         for (JButton[] buttons : buttonsBoard) {
             for (JButton button : buttons) {
                 button.setText(" ");
+                button.setBackground(Color.gray);
             }
         }
 
+        gameOver = false;
         player = "X";
     }
 }
